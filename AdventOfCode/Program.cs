a3261020427afe7cd0f4f189f03794e6c0d01221
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2020-12-01 07:05:20 +0100

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length < 3)
+			{
+				Console.WriteLine("AdventOfCode [Year] [Day] [Part] (Input)");
+				return;
+			}
+
+			var year = int.Parse(args[0], CultureInfo.InvariantCulture);
+			var day = int.Parse(args[1], CultureInfo.InvariantCulture);
+			var part = int.Parse(args[2], CultureInfo.InvariantCulture);
+			var init = args.Length == 4
+				? new[] { File.ReadAllText(args[3]) }
+				: new[] { GetEmbeddedInput(year, day) };
+
+			var type = Type.GetType($"AdventOfCode.Year{year}.Day{day}");
+			var instance = Activator.CreateInstance(type, init);
+			var method = type.GetMethod($"Part{part}");
+			var parameters = method.GetParameters().Select(p => p.RawDefaultValue);
+			var stopwatch = Stopwatch.StartNew();
+			var result = method.Invoke(instance, parameters.ToArray());
+
+			if (method.ReturnType.IsGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
+			{
+				result = result.GetType().GetMethod("GetAwaiter").Invoke(result, Type.EmptyTypes);
+				result = result.GetType().GetMethod("GetResult").Invoke(result, Type.EmptyTypes);
+			}
+
+			Console.WriteLine(stopwatch.Elapsed);
+			Console.WriteLine(result);
+		}
+
+		private static string GetEmbeddedInput(int year, int day)
+		{
+			using var stream = Assembly.GetExecutingAssembly()
+				.GetManifestResourceStream($"AdventOfCode.Year{year}.Inputs.Day{day}.txt");
+			using var reader = new StreamReader(stream);
+
+			return reader.ReadToEnd();
+		}
+	}
+}
