62d63ac49d76b25d66b3c9a4a9c31ed524ba8e02
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2020-12-02 14:23:32 +0100

    Day 2

---
+++ AdventOfCode.Tests/Year2020/Day2Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public class Day2Tests
+	{
+		public const string INPUT = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc";
+
+		[DataTestMethod]
+		[DataRow(INPUT, 2)]
+		public void Part1(string input, int expected)
+		{
+			Assert.AreEqual(expected, new Day2(input).Part1());
+		}
+
+		[DataTestMethod]
+		[DataRow(INPUT, 1)]
+		public void Part2(string input, int expected)
+		{
+			Assert.AreEqual(expected, new Day2(input).Part2());
+		}
+	}
+}
---
+++ AdventOfCode/Year2020/Day2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2020
+{
+	public class Day2
+	{
+		public static Regex entry = new Regex(@"^(?<min>\d+)-(?<max>\d+) (?<char>.): (?<password>.+)$", RegexOptions.Compiled);
+
+		private IEnumerable<PasswordEntry> entries;
+
+		public Day2(string input)
+		{
+			entries = input.Lines()
+				  .Select(line => entry.Match(line))
+				  .Select(match => new PasswordEntry(
+					  new Policy(
+						  int.Parse(match.Groups["min"].Value),
+						  int.Parse(match.Groups["max"].Value),
+						  match.Groups["char"].Value[0]),
+					  match.Groups["password"].Value));
+		}
+
+		public int Part1()
+		{
+			return entries.Count(entry => entry.Validate());
+		}
+
+		public int Part2()
+		{
+			return entries.Count(entry => entry.NewValidate());
+		}
+
+		public sealed record PasswordEntry(Policy Policy, string Password)
+		{
+			public bool Validate()
+			{
+				int count = Password.Count(c => c == Policy.Char);
+				return count >= Policy.Min && count <= Policy.Max;
+			}
+
+			public bool NewValidate()
+				=> Password[Policy.Min - 1] == Policy.Char
+				^ Password[Policy.Max - 1] == Policy.Char;
+		}
+
+		public sealed record Policy(int Min, int Max, char Char);
+	}
+}
