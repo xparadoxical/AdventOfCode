5e7f4a40fb13d44993146f69864837142e8ab402
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2020-12-05 06:55:21 +0100

    Day 5

---
+++ AdventOfCode.Tests/Year2020/Day5Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public sealed class Day5Tests
+	{
+		[DataTestMethod]
+		[DataRow("BFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL", 820)]
+		public void Part1(string input, int expected)
+		{
+			Assert.AreEqual(expected, new Day5(input).Part1());
+		}
+	}
+}
---
+++ AdventOfCode/Year2020/Day5.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2020
+{
+	public sealed class Day5
+	{
+		private readonly string[] passes;
+
+		public Day5(string input)
+		{
+			passes = input.Lines().ToArray();
+		}
+
+		public int Part1()
+		{
+			int highest = -1;
+
+			foreach (string pass in passes)
+			{
+				int id = SeatID(pass);
+				if (id > highest) 
+					highest = id;
+			}
+
+			return highest;
+		}
+
+		public int Part2()
+		{
+			var ids = passes.Select(pass => SeatID(pass)).OrderBy(id => id).ToArray();
+			for (int i = 0; i < ids.Length - 1; i++)
+				if (ids[i+1] != ids[i] + 1)
+					return ids[i] + 1;
+
+			throw new ArgumentException();
+		}
+
+		public static int SeatID(string boardingPass)
+		{
+			int x = 0, xl = 8;
+			int y = 0, yl = 128;
+
+			foreach (char c in boardingPass)
+			{
+				if (c == 'F')
+					yl /= 2;
+				else if (c == 'B')
+				{
+					yl /= 2;
+					y += yl;
+				}
+				else if (c == 'L')
+					xl /= 2;
+				else if (c == 'R')
+				{
+					xl /= 2;
+					x += xl;
+				}
+			}
+
+			if (!(xl == yl && xl == 1)) throw new ArgumentException();
+
+			return y * 8 + x;
+		}
+	}
+}
