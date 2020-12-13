907f882ed062022c4a96cd5b0a8098677715dfd9
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2020-12-13 08:50:40 +0100

    Day 7, finally.

---
+++ AdventOfCode.Tests/Year2020/Day7Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public sealed class Day7Tests
+	{
+		public const string Input =
+@"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+
+		[DataTestMethod]
+		[DataRow(Input, 4)]
+		public void Part1(string input, int expected)
+		{
+			Assert.AreEqual(expected, new Day7(input).Part1());
+		}
+
+		[DataTestMethod]
+		[DataRow(Input, 32)]
+		[DataRow(@"shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.", 126)]
+		public void Part2(string input, int expected)
+		{
+			Assert.AreEqual(expected, new Day7(input).Part2());
+		}
+	}
+}
---
+++ AdventOfCode/Year2020/Day7.cs
@@ -0,0 +1,209 @@
+﻿#nullable enable
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2020
+{
+	public sealed class Day7
+	{
+		private readonly BagDefinitions definitions;
+
+		public Day7(string input)
+		{
+			var bagDefinitionRegex = new Regex(@"(?<color>[a-z ]+) bags contain(?:(?<child> (?<count>\d+) (?<childColor>[a-z ]+) bags?(?:,|\.))+| no other bags\.)", RegexOptions.Compiled);
+			definitions = new BagDefinitions();
+
+			foreach (Match match in bagDefinitionRegex.Matches(input))
+			{
+				string color = match.Groups["color"].Value;
+
+				if (match.Groups["child"].Success)
+				{
+					List<BagChild> children = new(match.Groups["child"].Captures.Count);
+
+					for (int i = 0; i < children.Capacity; i++)
+					{
+						int count = int.Parse(match.Groups["count"].Captures[i].Value);
+						string childColor = match.Groups["childColor"].Captures[i].Value;
+						children.Add(new BagChild(childColor, count));
+					}
+
+					definitions.Add(new Bag(color, children));
+				}
+				else
+				{
+					definitions.Add(new Bag(color));
+				}
+			}
+		}
+
+		public int Part1()
+		{
+			int canHoldShinyGoldBag = 0;
+
+			foreach (var bagDef in definitions)
+			{
+				if (bagDef.Children is null)
+					continue;
+
+				Stack<IEnumerator<BagChild>> stack = new();
+				stack.Push(bagDef.Children.GetEnumerator());
+
+				do
+				{
+					var enumerator = stack.Peek();
+					if (enumerator.MoveNext())
+					{
+						string color = enumerator.Current.Color;
+						if (color == "shiny gold")
+						{
+							canHoldShinyGoldBag++;
+							break;
+						}
+
+						var newBag = definitions[color];
+						if (newBag.Children is not null)
+						{
+							stack.Push(newBag.Children.GetEnumerator());
+						}
+					}
+					else
+					{
+						_ = stack.Pop();
+						enumerator.Dispose();
+					}
+				}
+				while (stack.Count > 0);
+
+				while (stack.Count > 0)
+					stack.Pop()?.Dispose();
+			}
+
+			return canHoldShinyGoldBag;
+		}
+
+		public int Part2()
+		{
+			var shinyGold = definitions["shiny gold"];
+
+			Stack<(int?[] subnodeValueCache, IEnumerator<BagChild> enumerator, RefInt index)> stack = new();
+
+			{
+				var enumerator = shinyGold.Children!.GetEnumerator();
+				int?[] valueCache = new int?[shinyGold.Children!.Count()];
+				stack.Push((valueCache, enumerator, -1));
+			}
+
+			int count = -1;
+			do
+			{
+				var current = stack.Peek();
+				if (current.enumerator.MoveNext())
+				{
+					current.index++;
+					var bagChild = current.enumerator.Current;
+					var bagDef = definitions[bagChild.Color];
+
+					if (bagDef.Children is null)
+					{
+						current.subnodeValueCache[current.index] = bagChild.Count;
+					}
+					else
+					{
+						stack.Push((new int?[bagDef.Children.Count()], bagDef.Children.GetEnumerator(), -1));
+					}
+				}
+				else
+				{
+					_ = stack.Pop();
+					if (stack.Count > 0)
+					{
+						var parent = stack.Peek();
+						parent.subnodeValueCache[parent.index] = (current.subnodeValueCache.Sum() + 1) * parent.enumerator.Current.Count;
+					}
+					else
+					{
+						count = current.subnodeValueCache.Sum()!.Value;
+					}
+
+					current.enumerator.Dispose();
+				}
+			}
+			while (stack.Count > 0);
+
+			return count;
+		}
+
+		public sealed class RefInt
+		{
+			public int Value { get; set; }
+
+			public RefInt(int value) => Value = value;
+
+			public static RefInt operator ++(RefInt a)
+			{
+				a.Value++;
+				return a;
+			}
+
+			public static implicit operator RefInt(int i) => new RefInt(i);
+			public static implicit operator int(RefInt ri) => ri.Value;
+		}
+
+		public sealed class BagDefinitions : ICollection<Bag>
+		{
+			private readonly HashSet<Bag> colors = new();
+
+			public Bag this[string color]
+			{
+				get
+				{
+					var bag = colors.FirstOrDefault(b => b.Color == color);
+					if (bag is null)
+						throw new KeyNotFoundException();
+					return bag;
+				}
+			}
+
+			public int Count => colors.Count;
+			public bool IsReadOnly => false;
+
+			public void Add(Bag item)
+			{
+				if (!Contains(item))
+				{
+					bool b = colors.Add(item);
+				}
+			}
+
+			public void Clear() => colors.Clear();
+			public bool Contains(Bag item)
+			{
+				return colors.Any(bag =>
+				{
+					if (bag.Color != item.Color)
+						return false;
+					if (bag.Children is null ^ item.Children is null)
+						return false;
+					if (bag.Children is null && item.Children is null)
+						return true;
+					if (bag.Children!.Count() != item.Children!.Count())
+						return false;
+
+					return bag.Children!.Zip(item.Children!).All(p => p.First == p.Second);
+				});
+			}
+			public void CopyTo(Bag[] array, int arrayIndex) => colors.CopyTo(array, arrayIndex);
+			public IEnumerator<Bag> GetEnumerator() => colors.GetEnumerator();
+			public bool Remove(Bag item) => colors.Remove(item);
+			IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)colors).GetEnumerator();
+		}
+
+		public sealed record Bag(string Color, IEnumerable<BagChild>? Children = null);
+		public sealed record BagChild(string Color, int Count);
+	}
+}
