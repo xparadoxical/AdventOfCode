73c8bcca692589dba31140ce36596812e4ed5b74
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2020-12-20 05:16:23 +0100

    2020/18

---
+++ AdventOfCode.Tests/Year2020/Day18Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020
+{
+	[TestClass]
+	public sealed class Day18Tests
+	{
+		[DataTestMethod]
+		[DataRow("1 + 2 * 3 + 4 * 5 + 6", 71)]
+		[DataRow("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+		[DataRow("2 * 3 + (4 * 5)", 26)]
+		[DataRow("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+		[DataRow("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+		[DataRow("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+		public void Part1(string input, long expected)
+		{
+			Assert.AreEqual(expected, new Day18(input).Part1());
+		}
+
+		[DataTestMethod]
+		[DataRow("1 + 2 * 3 + 4 * 5 + 6", 231)]
+		[DataRow("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+		[DataRow("2 * 3 + (4 * 5)", 46)]
+		[DataRow("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+		[DataRow("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+		[DataRow("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+		public void Part2(string input, long expected)
+		{
+			Assert.AreEqual(expected, new Day18(input).Part2());
+		}
+	}
+}
---
+++ AdventOfCode/Year2020/Day18.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2020
+{
+	public sealed class Day18
+	{
+		private readonly IEnumerable<string> expressions;
+
+		public Day18(string input)
+		{
+			expressions = input.Lines();
+		}
+
+		public long Part1()
+		{
+			Operators operators = new()
+			{
+				new('+', 0, (l, r) => l + r),
+				new('*', 0, (l, r) => l * r)
+			};
+
+			return expressions.Sum(expr => Evaluate(expr, operators));
+		}
+
+		public long Part2()
+		{
+			Operators operators = new()
+			{
+				new('+', 1, (l, r) => l + r),
+				new('*', 0, (l, r) => l * r)
+			};
+
+			return expressions.Sum(expr => Evaluate(expr, operators));
+		}
+
+		public static long Evaluate(string input, Operators definitions)
+		{
+			Stack<char> operators = new();
+			Stack<long> numbers = new();
+
+			void DoOperation()
+			{
+				long r = numbers.Pop();
+				long l = numbers.Pop();
+				numbers.Push(definitions[operators.Pop()].Calculate(l, r));
+			}
+
+			foreach (char token in input)
+			{
+				if (char.IsDigit(token))
+					numbers.Push(token - '0');
+				else if (definitions.Contains(token))
+				{
+					while (operators.Count > 0
+						&& operators.Peek() is not '('
+						&& definitions[operators.Peek()].Precedence >= definitions[token].Precedence)
+					{
+						DoOperation();
+					}
+
+					operators.Push(token);
+				}
+				else if (token is '(')
+					operators.Push(token);
+				else if (token is ')')
+				{
+					while (operators.Peek() is not '(')
+						DoOperation();
+
+					if (operators.Peek() is '(')
+						operators.Pop();
+				}
+			}
+
+			while (operators.Count > 0)
+				DoOperation();
+
+			return numbers.Pop();
+		}
+
+		public sealed class Operators : List<Operator>
+		{
+			public Operator this[char c] => this.FirstOrDefault(op => op.Character == c) ?? throw new Exception($"Unknown operator {c}");
+			public bool Contains(char c) => this.Any(op => op.Character == c);
+		}
+
+		public sealed record Operator(char Character, int Precedence, Func<long, long, long> Calculate);
+	}
+}
